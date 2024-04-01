083a9a2f06e01b18e58ac729d1b31647dba524c7
phicos-ministry <phicosdev@gmail.com>
2024-04-01 16:10:03 +0000

    fix meta name and FAQ

--- resources/views/layouts/front/app-beranda.blade.php
+++ resources/views/layouts/front/app-beranda.blade.php
@@ -6,6 +6,12 @@
 
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="SemiColonWeb" />
+    <meta name="description"
+        content="Selamat datang di Portal Direktorat Kepala Sekolah, Pengawas Sekolah dan Tenaga Kependidikan. Temukan berbagai informasi terkait pengelolaan kepala sekolah, pengawas sekolah dan tenaga kependidikan di Indonesia di sini."/>
+    <meta name="keywords"
+        content="Direktorat KSPS Tendik, Portal Kepala Sekolah, Portal Pengawas Sekolah, Portal Tenaga Kependidikan, informasi kependidikan, pengelolaan sekolah"/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="author" content="DirektoratKSPSTendik"/>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     @include('layouts.front.css')
@@ -55,47 +61,52 @@
             </div>
             <button  type="submit" class="btn btn-primary btn-simpan w-100">Kirim</button>
         </form> --}}
-        <form action="{{ route('faq.store') }}"  method="post" name="form-store" id="form-store"  class="w-100 mb-0">
+        <form action="{{ route('faq.store') }}" method="post" name="form-store" id="form-store" class="w-100 mb-0">
             @csrf
             <div class="row">
                 <div class="form-group col-6">
-                    <select class="form-control" id="id_kategori_faq" name="id_kategori_faq" data-placeholder="Pilih Kategori">
+                    <select class="form-control" id="id_kategori_faq" name="id_kategori_faq"
+                        data-placeholder="Pilih Kategori" required>
                         <option>Pilih Kategori</option>
                         <option disabled>Kategori</option>
-                        @foreach ( $kategori_faq as $data )
-                            <option value="{{$data->id}}">{{$data->nama_kategori}}</option>
+                        @foreach ($kategori_faq as $data)
+                            <option value="{{ $data->id }}">{{ $data->nama_kategori }}</option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group col-6">
-                    <select class="form-control"  id="id_keperluan_faq" name="id_keperluan_faq" data-placeholder="Pilih Keperluan">
+                    <select class="form-control" id="id_keperluan_faq" name="id_keperluan_faq"
+                        data-placeholder="Pilih Keperluan" required>
                         <option>Pilih Keperluan</option>
                         <option disabled>Keperluan</option>
-                        @foreach ( $keperluan_faq as $item )
-                            <option value="{{$item->id}}">{{$item->nama_keperluan}}</option>
+                        @foreach ($keperluan_faq as $item)
+                            <option value="{{ $item->id }}">{{ $item->nama_keperluan }}</option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group col-12">
-                    <input type="text" class="form-control" id="nama" name="nama"  placeholder="Nama Lengkap">
+                    <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap" required>
                 </div>
                 <div class="form-group col-12">
-                    <input type="email" class="form-control" id="email" name="email"  placeholder="Email anda">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email anda" required>
                 </div>
                 <div class="form-group col-6">
-                    <input type="text" class="form-control" id="nip" name="nip"  placeholder="NIP/NIK">
+                    <input type="number" class="form-control" id="nip" name="nip" placeholder="NIP/NIK"
+                        pattern="[0-9]*" required>
                 </div>
                 <div class="form-group col-6">
-                    <input type="text" class="form-control" id="instansi" name="instansi"  placeholder="Instansi">
+                    <input type="text" class="form-control" id="instansi" name="instansi" placeholder="Instansi" required>
                 </div>
                 <div class="form-group col-6">
-                    <input type="text" class="form-control" id="jabatan" name="jabatan"  placeholder="Jabatan">
+                    <input type="text" class="form-control" id="jabatan" name="jabatan" placeholder="Jabatan" required>
                 </div>
                 <div class="form-group col-6">
-                    <input type="text" class="form-control" id="nomor_hp" name="nomor_hp"  placeholder="Nomor HP">
+                    <input type="number" class="form-control" id="nomor_hp" name="nomor_hp" placeholder="Nomor HP"
+                        pattern="[0-9]*" required>
                 </div>
                 <div class="form-group col-12">
-                    <textarea class="form-control" id="pertanyaan" name="pertanyaan" rows="3" style="height: 86px;" placeholder="Isi pertanyaan anda"></textarea>
+                    <textarea class="form-control" id="pertanyaan" name="pertanyaan" rows="3" style="height: 86px;"
+                        placeholder="Isi pertanyaan anda" required></textarea>
                 </div>
             </div>
             <button type="submit" class="btn btn-primary btn-simpan w-100">Kirim</button>
@@ -114,17 +125,19 @@
                         <div class="entry mb-4">
                             <div class="grid-inner row no-gutters p-0">
                                 <div class="entry-image col-3">
-                                    <a href="{{$podcast->link_podcast}}" target="_blank">
+                                    <a href="{{ $podcast->link_podcast }}" target="_blank">
                                         <img src="{{ asset('podcast/' . $podcast->gambar) }}" alt="thumbnail_podcast">
                                     </a>
                                 </div>
                                 <div class="col-9 pl-3">
                                     <div class="entry-title title-md text-clamp-2">
-                                        <h6 class="mb-1"><a href="{{$podcast->link_podcast}}" target="_blank">{{ $podcast->judul }}</a></h6>
+                                        <h6 class="mb-1"><a href="{{ $podcast->link_podcast }}"
+                                                target="_blank">{{ $podcast->judul }}</a></h6>
                                     </div>
                                     <div class="entry-meta mb-2 mt-0">
                                         <ul>
-                                            <li><a href="#"><i class="icon-users"></i> {{$podcast->jumlah_lihat ?? 0}} penonton</a>
+                                            <li><a href="#"><i class="icon-users"></i>
+                                                    {{ $podcast->jumlah_lihat ?? 0 }} penonton</a>
                                             <li><a href="#"><i
                                                         class="icon-calendar3"></i>{{ $podcast->date }}</a>
                                             </li>
